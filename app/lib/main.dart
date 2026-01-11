279e2ddcd20666966c1dd9b5b04fb6b1fecc67d1
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2026-01-11 18:23:59 +0100

    refactor: remove flutter native splash

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mocc/router/router.dart';
 import 'package:mocc/theme/theme.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(
     EasyLocalization(
