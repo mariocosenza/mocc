15960532f7b1b6585eab074af8c64bf73dabda5b
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2026-01-11 16:26:14 +0100

    feat: auth service

---
+++ app/lib/auth/auth_service_factory.dart
@@ -0,0 +1,6 @@
+import 'auth_service.dart';
+import 'auth_service_stub.dart'
+    if (dart.library.html) 'auth_service_web.dart'
+    if (dart.library.io) 'auth_service_mobile.dart';
+
+AuthService createAuthService() => createAuthServiceImpl();
---
+++ app/lib/auth/auth_service_stub.dart
@@ -0,0 +1,4 @@
+import 'auth_service.dart';
+
+AuthService createAuthServiceImpl() =>
+    throw UnsupportedError('No AuthService implementation for this platform.');
