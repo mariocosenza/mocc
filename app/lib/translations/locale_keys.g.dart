916a766afbad6150768cd146e7e91e61a4999c1f
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2026-01-14 20:16:20 +0100

    feat(frontend): add new translations strings for settings and onboard

--- app/lib/translations/locale_keys.g.dart
+++ app/lib/translations/locale_keys.g.dart
@@ -15,6 +15,12 @@ abstract class  LocaleKeys {
   static const step2_description = 'step2_description';
   static const step3_title = 'step3_title';
   static const step3_description = 'step3_description';
+  static const privacy_agreement = 'privacy_agreement';
+  static const llm_user_allergy_intolerance = 'llm_user_allergy_intolerance';
+  static const peaple_in_family = 'peaple_in_family';
+  static const saving = 'saving';
+  static const currency = 'currency';
+  static const save = 'save';
   static const items_count = 'items_count';
 
 }
