f817a914205d0f36571e2bd3c3af68956367eefb
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2026-01-15 20:01:05 +0100

    feat(frontend): social screen template

---
+++ app/lib/views/social_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:mocc/widgets/social_post_list_view.dart';
+
+class SocialScreen extends StatefulWidget {
+  const SocialScreen({super.key});
+
+  @override
+  State<SocialScreen> createState() => _SocialScreenState();
+}
+
+class _SocialScreenState extends State<SocialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(children: const [Expanded(child: SocialPostListiView())]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 110),
+        child: Tooltip(
+          message: 'Increment',
+          preferBelow: false,
+          child: FloatingActionButton(
+            onPressed: () {},
+            elevation: 24,
+            highlightElevation: 28,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(999),
+            ),
+            child: const Icon(Icons.post_add, size: 28),
+          ),
+        ),
+      ),
+    );
+  }
+}
